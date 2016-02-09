ceb686dcebd35012d6e707803ac0d98cac80094b
HerrickSpencerMSFT <IOTMe@HerrickSpencer.com>
2016-02-09 14:59:07 -0800

    Add download event handling, and test button for download on Windows (no openSSL on windows)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QMessageBox>
+#include <QProcess>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->webView->page()->setForwardUnsupportedContent(true);
+    connect(ui->webView->page(),SIGNAL(downloadRequested(QNetworkRequest)),this,SLOT(download(QNetworkRequest)));
+    connect(ui->webView->page(),SIGNAL(unsupportedContent(QNetworkReply*)),this,SLOT(unsupportedContent(QNetworkReply*)));
+
     //ui->webView->load(QUrl("https://auth.windowsondevices.com"));
     ui->webView->load(QUrl("https://insider.windows.com"));
     connect( ui->webView, SIGNAL(urlChanged(QUrl)), this, SLOT(UrlChanged()));
@@ -20,4 +25,71 @@ MainWindow::~MainWindow()
 void MainWindow::UrlChanged()
 {
     QMessageBox::critical(this,"URL Change", tr("url changed to %1").arg(ui->webView->url().toString()));
+}
+
+void MainWindow::download(const QNetworkRequest &request){
+    qDebug()<<"Download Requested: "<<request.url();
+    if (isIsoUrl(request.url()))
+    {
+        if(DownloadIso(request.url()))
+        {
+            qDebug()<<"Successfull Download";
+        }
+        else
+        {
+            qDebug()<<"Download Failed";
+        }
+    }
+}
+
+void MainWindow::unsupportedContent(QNetworkReply * reply){
+    qDebug()<<"Unsupported Content: "<<reply->url();
+    if (isIsoUrl(reply->url()))
+    {
+        if (DownloadIso(reply->url()))
+        {
+            qDebug()<<"Successfull Download";
+        }
+        else
+        {
+            qDebug()<<"Download Failed";
+        }
+    }
+}
+
+bool MainWindow::isIsoUrl(QUrl url)
+{
+    QString urlStr = url.toString();
+    QRegularExpression re("\\.iso", QRegularExpression::CaseInsensitiveOption);
+    if (urlStr.contains(re))
+    {
+        return true;
+    }
+    return false;
+}
+
+bool MainWindow::DownloadIso(QUrl url){
+    bool result = false;
+    QString isoUrl = url.toString();
+    isoUrl.remove(QRegExp("QUrl(\""));
+    isoUrl.remove(QRegExp("\")"));
+    qDebug()<<"To download: "<<isoUrl;
+
+    QProcess wgetProcess;
+    QString cmd = "wget -O /temp/Wow.iso " + isoUrl;
+    qDebug()<<"Running: "<<cmd;
+    wgetProcess.start(cmd);
+    wgetProcess.waitForFinished();
+    result = wgetProcess.exitStatus() == QProcess::NormalExit && wgetProcess.exitCode() == 0;
+    QString resultStr = QString::number(result);
+    qDebug()<<"Result: "<<resultStr;
+    qDebug()<<"ErrorStr: "<<wgetProcess.errorString();
+    wgetProcess.close();
+
+    return result;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->webView->load(QUrl("http://localhost/Iso/14262.1000.160206-1700.RS1_RELEASE_IOTCoreRPi_armFRE.ISO"));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QNetworkRequest>
+#include <QNetworkReply>
 
 namespace Ui {
 class MainWindow;
@@ -16,8 +18,15 @@ public:
     ~MainWindow();
 public slots:
     void UrlChanged();
+    void download(const QNetworkRequest &request);
+    void unsupportedContent(QNetworkReply *reply);
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    bool DownloadIso(QUrl url);
+    bool isIsoUrl(QUrl url);
 };
 
 #endif // MAINWINDOW_H
