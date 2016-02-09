d38468cb721811a9b66a790f9eda13b42ee8479d
Herrick Spencer <HerrickSpencerMSFT>
2016-02-08 22:36:25 -0800

    Signal and slot on url change, messagebox pop

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -9,9 +9,15 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     //ui->webView->load(QUrl("https://auth.windowsondevices.com"));
     ui->webView->load(QUrl("https://insider.windows.com"));
+    connect( ui->webView, SIGNAL(urlChanged(QUrl)), this, SLOT(UrlChanged()));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::UrlChanged()
+{
+    QMessageBox::critical(this,"URL Change", tr("url changed to %1").arg(ui->webView->url().toString()));
 }
